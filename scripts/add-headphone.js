ec4e705912570e2ba6485dcbfef04ae66565e861
yamen22-dev <120108370+yamen22-dev@users.noreply.github.com>
2023-01-02 00:51:09 +0100

    now will alert giving if the added product already in the database.

--- scripts/add-headphone.js
+++ scripts/add-headphone.js
@@ -1,44 +1,23 @@
-function main() {
-    document.querySelector("form").addEventListener("submit", (e)=> {
-        e.preventDefault();
-        readFields();
-        window.location.replace("../admin.html");
-    } );
-    const previewButton = document.getElementById("preview-button");
-    const previewPlace = document.getElementById("preview-place");
-    previewButton.addEventListener("click", () => {
-        const url = document.getElementById("url").value;
-        previewPlace.src = url;
-    });
-}
+document.querySelector("form").addEventListener("submit", (e)=> {
+    e.preventDefault();
+    readFields();
+} );
+const previewButton = document.getElementById("preview-button");
+const previewPlace = document.getElementById("preview-place");
+previewButton.addEventListener("click", () => {
+    const url = document.getElementById("url").value;
+    previewPlace.src = url;
+});
 function readLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key));
 }
 function setLocalStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
-function checkInputs(inputs) {
-    for (const input of inputs) {
-        if (input === '') {
-            if (document.getElementById("oppasAlert") !== null) {
-                document.getElementById("oppasAlert").remove();
-            }
-            document.getElementById("go-back").innerHTML +=
-                `<div class="alert alert-danger my-2" id="oppasAlert" role="alert">Je moet alle velden invullen!!!!</div>`;
-            return false;
-        }
-    }
-    if (document.getElementById("oppasAlert") !== null) {
-        document.getElementById("oppasAlert").remove();
-    }
-    return true;
-}
 function checkDubble (item) {
     for (const element of readLocalStorage('data')) {
         if (JSON.stringify(element) === JSON.stringify(item)) {
             return false;
-        } else {
-            console.log(JSON.stringify(element), JSON.stringify(item));
         }
     }
     return true;
@@ -53,11 +32,13 @@ function readFields() {
     const url = document.getElementById("url").value;
     const specs = { "noise-cancellation": noise, "draag-comfort": draag, "geluids-kwaliteit": geluids };
     const inputs = { "id": id, "name": name, "specs": specs, "price": price, "url": url };
-    if (checkInputs([id,price,name,noise,draag,geluids,url,specs]) && checkDubble(inputs)) {
+    if (checkDubble(inputs)) {
         let data = readLocalStorage("data");
         data.push(inputs);
-        setLocalStorage("data",data)
+        setLocalStorage("data",data);
+        alert("You have successfully added headphone");
+        window.location.replace("../admin.html");
+    } else {
+        alert("You have already this headphone in your database");
     }
 }
-
-main();
